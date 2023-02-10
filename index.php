08fbebe30585d2961b449bf952c383dd424348c9
Giacomo Pedemonte <giacomopedemonte@libero.it>
2023-02-10 18:11:28 +0100

    My personal CV

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Resume of Giacomo Pedemonte</title>
+    <style>
+      /* Add your CSS styling here */
+      body {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+        padding: 1em;
+      }
+
+      h1 {
+        font-size: 36px;
+        margin-bottom: 0.5em;
+      }
+
+      h2 {
+        font-size: 24px;
+        margin-bottom: 0.5em;
+      }
+
+      .section {
+        margin-bottom: 2em;
+      }
+
+      .section-header {
+        font-weight: bold;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Resume of Giacomo Pedemonte</h1>
+    <div class="section">
+      <h2 class="section-header">Contact Information</h2>
+      <p>
+        <strong>Name:</strong> Giacomo Pedemonte<br>
+        <strong>Date of Birth:</strong> December 18, 1999<br>
+        <strong>Address:</strong> [Insert your address]<br>
+        <strong>Phone Number:</strong> [Insert your phone number]<br>
+        <strong>Email:</strong> [Insert your email address]
+      </p>
+    </div>
+    <div class="section">
+      <h2 class="section-header">Education</h2>
+      <p>
+        <strong>Diploma in Computer Science from Italo Calvino Technical High School</strong><br>
+        [Insert the date of graduation]<br>
+        <em>Score:</em> 83/100
+      </p>
+    </div>
+    <div class="section">
+      <h2 class="section-header">Technical Skills</h2>
+      <ul>
+        <!-- Insert your technical skills, such as programming, web development, databases, cybersecurity, etc. -->
+        <li>Programming</li>
+        <li>Web Development</li>
+        <li>Databases</li>
+        <li>Cybersecurity</li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2 class="section-header">Work Experience</h2>
+      <ul>
+        <!-- Insert any previous work experiences, including the name of the company, your role, and the dates of employment -->
+        <li>
+          <strong>Software Developer at XYZ Inc.</strong><br>
+          [Insert dates of employment]<br>
+          [Insert a brief description of your role and responsibilities]
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2 class="section-header">Languages</h2>
+      <ul>
+        <!-- Insert the languages you speak and your level of proficiency -->
+                <li>Italian (native speaker)</li>
+        <li>English (fluent)</li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2 class="section-header">References</h2>
+      <ul>
+        <!-- Insert the names and contact information of individuals who can serve as professional references -->
+        <li>
+          <strong>John Doe</strong><br>
+          [Insert job title and company name]<br>
+          [Insert email address and phone number]
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
